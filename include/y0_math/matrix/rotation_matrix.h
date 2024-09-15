621ce5e9705b9cba3da3851d01278e00e7b60cd3
y0ub <y0lit7515@gmail.com>
2024-09-16 08:35:48 +0900

    update: namespace

--- include/y0_math/matrix/rotation_matrix.h
+++ include/y0_math/matrix/rotation_matrix.h
@@ -1,9 +1,13 @@
 #ifndef Y0_MATH_INCLUDE_Y0_MATH_MATRIX_ROTATION_MATRIX_H
 #define Y0_MATH_INCLUDE_Y0_MATH_MATRIX_ROTATION_MATRIX_H
 
-template<typename T>
-struct RotationMatrix<T, typename std::enable_if<std::is_arithmetic<T>::value>::type> {
-  public:
-};
+namespace y0_math {
+  namespace matrix {
+    template<typename T>
+      struct RotationMatrix<T, typename std::enable_if<std::is_arithmetic<T>::value>::type> {
+        public:
+      };
+  } // namespace matrix
+} // namespace y0_math
 
 #endif
